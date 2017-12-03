4caeefb673989a6976b4547f8d1b0d3e0c9337b3
frank402 <frank40202@gmail.com>
2017-12-03 19:50:50 +0800

    model todo.rb modified

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,26 @@
 class Todo < ApplicationRecord
+	  # 驗證：欄位必須有值
+  # 預設不允許空白字元，可用 allow_blank: :true 開啟允許空白
+  validates_presence_of :title, :due_date
+  # 驗證：長度 20 個字符
+  # 英文是 20 個字母，中文則是 20 個字
+  validates :title, length: { maximum: 20 }
+
+  # 判斷是否能夠刪除
+  # 判斷條件可能有很多，所以將各項判斷分拆到 private method
+  def can_destroy?
+    # 沒過期的話，回傳 true = 可刪除
+    !overdue?
+  end
+
+  # 宣告以下為 private method
+  # private_method 無法被直接使用
+  # `todo.overdue?` 會跳出錯誤: NoMethodError: private method `overdue?`'
+  # 可以用 Ruby 原生的 Kernel#send 來呼叫 private_method: todo.send(:overdue?)
+  private
+
+  # 如果 due_date 比今天小，則回傳 true = 過期
+  def overdue?
+    due_date < Date.today
+  end
 end
