5f306e437f90915c72bbf69382160ab8dfb28afe
frank402 <frank40202@gmail.com>
2017-12-08 22:40:43 +0800

    complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Todo.destroy_all
+
+# 在 iterm 命令列輸入 `rails db:seed` 生成原始資料
+todo_list = [
+  {
+    title:    '製作小工具',
+    due_date: '2017-11-05'
+  },
+  {
+    title:    'Piano class',
+    due_date: '2017-11-12'
+  },
+  {
+    title:    'Buy Milk',
+    due_date: '2017-11-18'
+  },
+  {
+    title:    'Booking tickets',
+    due_date: '2017-11-19'
+  },
+  {
+    title:    'Final project',
+    due_date: '2018-01-01'
+  },
+]
+
+todo_list.each do |todo|
+  Todo.create(
+    title:    todo[:title],
+    due_date: todo[:due_date]
+  )
+end
